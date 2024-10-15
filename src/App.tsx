5731ec9e2eafdc5116c728846dc6bdee6cb1e7bc
namjh654 <namjh654@naver.com>
2024-10-15 22:28:21 +0900

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h1>봉봉이의 블로그</h1>
+        <p>This is the first post on my blog. Stay tuned for more updates!</p>
+        <a className="App-link" href="#" onClick={(e) => e.preventDefault()}>
+          Read More
         </a>
       </header>
+      <main className="App-content">
+        <article className="Post">
+          <h2>Blog Post Title</h2>
+          <p>
+            This is a short introduction to the blog post. Click below to read
+            more!
+          </p>
+          <button className="Post-button">Read Full Post</button>
+        </article>
+      </main>
     </div>
   );
 }
