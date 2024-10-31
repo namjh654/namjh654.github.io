834aa30110309c62a96b0f2bda5b819fca0b92d6
namjh654 <namjh654@naver.com>
2024-10-31 13:42:20 +0900

    drawer 네비게이션

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,4 @@
-import React from "react";
 import "../styles/Pages/Home.css";
-import { FaUser, FaChartLine, FaTasks, FaBell } from "react-icons/fa";
 
 const Home = () => {
   return <div>sd</div>;
--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,5 +1,24 @@
-const schedule = () => {
-  return <div>schedule</div>;
+import { useState } from "react";
+import Calendar from "react-calendar";
+// import "react-calendar/dist/Calendar.css";
+import "../styles/Pages/Schedule.css";
+
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+const Schedule = () => {
+  const [value, setValue] = useState<Value>(new Date());
+
+  const onChange = (value: Value) => {
+    setValue(value);
+    console.log("val", value);
+  };
+
+  return (
+    <div className="container">
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
 };
 
-export default schedule;
+export default Schedule;
